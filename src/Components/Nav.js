009922baca7bbc04a4a1aa5366806441748abbf8
Seena Nadimi <seenanadimi@gmail.com>
2021-07-08 15:54:07 -0700

    ADD: added nav and main components

---
+++ src/Components/Nav.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Nav = () => {
+  const styles = {
+    container: {
+      height: "10vh",
+      width: "100%",
+      backgroundColor: "white",
+      display: "flex",
+    },
+    logo: {
+      width: "50%",
+      height: "100%",
+      marginLeft: "20px",
+    },
+    nav: {
+      width: "50%",
+      height: "100%",
+      display: "flex",
+      justifyContent: "space-evenly",
+      alignItems: "center",
+    },
+    
+    
+  };
+
+  return (
+  
+  <div style={styles.container}>
+    <div style={styles.logo}>
+      <h2>TEAM 2</h2>
+    </div>
+    <div style={styles.nav}>
+      <p>NAV</p>
+      <p>NAV</p>
+      <p>NAV</p>
+    </div>
+  </div>
+
+)};
+
+export default Nav;
