a567b967697ad33a2faae81f11d5fb9a9daa31cd
Franco <phanfranco@yahoo.com>
2021-07-09 14:29:29 -0700

    ADD: Added react router dom.

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,20 +1,36 @@
-import React from 'react'
+import React from "react";
+import Glassmorphism from "./Glassmorphism";
+import Flatdesign from "./Flatdesign";
+import FlatCard from "./Flatcard";
+import Neumorphism from "./Neumorphism.js";
 
-const Main = (props) => {
+const Main = () => {
   const styles = {
     display: "flex",
     flexFlow: "wrap row",
-    justifyContent:"space-around",
+    justifyContent: "center",
     alignItems: "center",
     width: "100%",
     minHeight: "80vh",
+    h1: {
+      width: "50%",
+      color: "white",
+      marginLeft: "5%",
+    },
   };
 
   return (
     <div style={styles}>
-      {props.children}
-    </div>
-  )
-}
+      <Neumorphism />
+      <h1 style={styles.h1}>
+        Design is an integral part of web application development.
+      </h1>
 
-export default Main
+      {/* <Glassmorphism />
+      <Flatdesign />
+      <FlatCard /> */}
+    </div>
+  );
+};
+
+export default Main;
