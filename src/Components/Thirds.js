fdd01f814a02168f4687795f6b5d3e8d71f4d462
Franco <phanfranco@yahoo.com>
2021-07-12 17:00:30 -0700

    CONFLICT: FIXXED

---
+++ src/Components/Thirds.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Thirds = () => {
+  return (
+    <div>
+      <div>
+        <p>A photograph is the pause button of life</p>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Thirds;
