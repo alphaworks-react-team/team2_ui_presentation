81e45570728919824bbda40945e665db0ceb72f7
MarkGee45 <moel55.mg@gmail.com>
2021-07-16 09:34:31 -0700

    Add: added flexing and styling

--- src/Components/Neumorphism.js
+++ src/Components/Neumorphism.js
@@ -1,15 +1,10 @@
 import React from "react";
 import drake from "../image/drake.jpeg";
-import menu from "../image/menu.png";
-import arrow from "../image/big-left-arrow.png";
-import rewind from "../image/clear-rewind.png";
-import pause from "../image/pause-24.png";
-import forward from "../image/fast-forward-24.png";
 import weekend from "../image/The_Weekend2.jpeg";
 import aiko from "../image/jhene-aiko-11.jpeg";
-import { CgDisc } from "react-icons/cg";
-import { BsPlus, BsHeart, BsDownload, BsThreeDots, BsArrowRepeat, BsPauseFill, BsShuffle } from "react-icons/bs";
-import { FiChevronLeft, FiSkipBack, FiSkipForward } from "react-icons/fi";
+import { CgDisc, CgMenuGridO } from "react-icons/cg";
+import { BsPlus, BsHeart, BsDownload, BsThreeDots, BsArrowRepeat, BsPauseFill, BsShuffle, BsArrowLeftShort } from "react-icons/bs";
+import { FiChevronLeft, FiSkipBack, FiSkipForward, FiFastForward, FiRewind } from "react-icons/fi";
 
 
 const Neumorphism = () => {
@@ -31,40 +26,37 @@ const Neumorphism = () => {
       padding: "5px",
       border: "solid 1px #ed4575",
       borderRadius: "25px",
-      height: "70vh",
-      width: "15vw",
+      height: "600px",
+      width: "288px",
     },
 
     topControls: {
       display: "flex",
-      alignItems: "center",
-      justifyContent: "space-between",
+      alignItems: "flex-end",
+      justifyContent: "space-around",
+      height: "60px",
     },
     arrow: {
       height: "40px",
       width: "40px",
       borderRadius: "50%",
-      margin: "10px",
+      color: "#8b0e31",
       border: "solid 1px #ed4575",
-      backgroundImage: `url(${arrow})`,
-      backgroundRepeat: "no-repeat",
-      backgroundPosition: "center",
       backgroundSize: "25px 25px",
       boxShadow: "4px 4px 8px 0 rgba(0, 0, 0, 0.25), -5px -5px 9px 0 rgb(241, 142, 168)",
     },
     playing: {
       color: "#8b0e31",
       fontSize: "10px",
+      alignSelf: "center",
     },
     menu: {
-      height: "40px",
-      width: "40px",
+      height: "20px",
+      width: "20px",
+      color: "#8b0e31",
+      padding: "10px",
       borderRadius: "50%",
-      margin: "10px",
       border: "solid 1px #ed4575",
-      backgroundImage: `url(${menu})`,
-      backgroundRepeat: "no-repeat",
-      backgroundPosition: "center",
       boxShadow: "4px 4px 8px 0 rgba(0, 0, 0, 0.25), -5px -5px 9px 0 rgb(241, 142, 168)",
     },
 
@@ -129,41 +121,34 @@ const Neumorphism = () => {
       marginTop: "80px",
     },
     rewind: {
-      height: "60px",
-      width: "60px",
+      height: "40px",
+      width: "40px",
       borderRadius: "50%",
-      margin: "10px",
+      padding: "10px",
+      color: "#8b0e31",
       border: "solid 1px #EA2E63",
-      backgroundImage: `url(${rewind})`,
-      backgroundRepeat: "no-repeat",
-      backgroundPosition: "center",
       boxShadow: "4px 4px 8px 0 rgba(0, 0, 0, 0.25), -5px -5px 9px 0 rgb(241, 142, 168)",
     },
     pause: {
-      height: "60px",
-      width: "60px",
+      height: "40px",
+      width: "40px",
       borderRadius: "50%",
-      margin: "10px",
+      padding: "10px",
+      color: "#8b0e31",
       border: "solid 1px #EA2E63",
-      backgroundImage: `url(${pause})`,
-      backgroundRepeat: "no-repeat",
-      backgroundPosition: "center",
       boxShadow: "4px 4px 8px 0 rgba(0, 0, 0, 0.25), -5px -5px 9px 0 rgb(241, 142, 168)",
     },
     forward: {
-      height: "60px",
-      width: "60px",
+      height: "40px",
+      width: "40px",
       borderRadius: "50%",
-      margin: "10px",
+      padding: "10px",
+      color: "#8b0e31",
       border: "solid 1px #EA2E63",
-      backgroundImage: `url(${forward})`,
-      backgroundRepeat: "no-repeat",
-      backgroundPosition: "center",
       boxShadow: "4px 4px 8px 0 rgba(0, 0, 0, 0.25), -5px -5px 9px 0 rgb(241, 142, 168)",
     },
     rightside: {
       display: "flex",
-      justifyContent: "flex-start",
       alignItems: "center",
     },
     imageRight: {
@@ -174,8 +159,8 @@ const Neumorphism = () => {
       backgroundPosition: "center",
       border: "solid 2px #333333",
       borderRadius: "45px",
-      height: "70vh",
-      width: "17vw",
+      height: "600px",
+      width: "288px",
     },
     farside: {
         display: "flex",
@@ -184,33 +169,24 @@ const Neumorphism = () => {
         alignContent: "space-between",
     },
     farright: {
-        // border: "solid 3px rgba(255, 255, 255, .4)",
-        border: "2px solid rgba(255,255,255)",
+        border: "3px solid rgba(255,255,255)",
         borderRadius: "25px",
-        height: "70vh",
-        width: "17vw",
-        background: "rgba(255,179,255, .45)",
-        
+        height: "600px",
+        width: "288px",
+        background: "rgba(255,179,255, .4)",
         boxShadow: "4px 4px 8px 0 rgba(0, 0, 0, 0.25), -4px -4px 8px 0 rgb(163, 80, 175)",
     },
     topFarControls: {
         display: "flex",
-        alignItems: "center",
+        alignItems: "flex-end",
+        alignContent: "flex-end",
         justifyContent: "space-around",
-        marginTop: "15px",
+        height: "60px",
       },
       arrowFar: {
         height: "40px",
         width: "40px",
-        // borderRadius: "50%",
-        // margin: "10px",
-        // border: "solid 5px #ffccdd",
         color: "#ffccdd",
-        // backgroundImage: `url(${less})`,
-        // backgroundRepeat: "no-repeat",
-        // backgroundPosition: "center",
-        // backgroundSize: "25px 25px",
-        // boxShadow: "4px 4px 8px 0 rgba(26, 0, 8, 0.3), -2px -2px 5px 0 rgb(255, 230, 238)",
       },
       playingFar: {
         display: "flex",
@@ -221,7 +197,7 @@ const Neumorphism = () => {
         height: "20px",
         padding: "10px",
         backgroundColor: "rgba(172,79,168, .25)",
-        // border: "solid 2px rgba(255, 204, 221, .3)",
+        border: "solid 2px rgba(255, 204, 221, .3)",
         color: "#ffccdd", 
       },
       disc: {
@@ -232,18 +208,15 @@ const Neumorphism = () => {
       menuFar: {
         height: "40px",
         width: "40px",
-        // borderRadius: "50%",
-        // margin: "10px",
-        // border: "solid 5px #ffccf2",
         color: "#ffccdd",
-        // boxShadow: "4px 4px 8px 0 rgba(51, 0, 39, 0.25), -2px -2px 5px 0 rgb(255, 230, 249)",
       },
 
     middleControlsFar: {
         display: "flex",
         flexDirection: "column",
-        alignContent: "space-between",
         alignItems: "center",
+        justifyContent: "center",
+        height: "300px",
       },
       artistImageFar: {
         backgroundImage: `url(${aiko})`,
@@ -252,46 +225,49 @@ const Neumorphism = () => {
         backgroundPosition: "center",
         height: "200px",
         width: "200px",
-        marginTop: "20px",
         borderRadius: "25px",
         border: "solid 7px rgba(255, 204, 221, .4)",
-        
-        // boxShadow: "7px 7px 12px 0 rgba(23, 3, 8, 0.5), -3px -3px 7px 0 rgb(247, 187, 203)",
+        boxShadow: "7px 7px 12px 0 rgba(23, 3, 8, 0.5), -3px -3px 7px 0 rgb(247, 187, 203)",
+      },
+      album: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-between",
+        height: "35px",
       },
       titleFar: {
         color: "#ffccdd",
-        marginTop: "30px",
-        marginBottom: "0",
-        // textShadow: "3px 3px 6px rgb(47, 4, 16)",
+        fontSize: "20px",
+        textShadow: "3px 3px 6px rgb(47, 4, 16)",
       },
       artistFar: {
         color: "#ffccdd",
-        marginTop: "0",
-        fontSize: "10px"
+        fontSize: "15px",
       },
       timeLapseFar: {
         display: "flex",
         flexDirection: "row",
-        justifyContent: "space-evenly",
-        alignItems: "center",
-        // width: "220px",
+        justifyContent: "space-around",
+        alignItems: "flex-end",
         color: "#ffccdd",
         fontSize: "15px",
-        // marginTop: "10px",
+        height: "35px",
       },
   
       dialFar: {
         backgroundImage: "linear-gradient(to right, #8455C7 , #e3d9f2)",
         height: "6px",
-        width: "200px",
+        width: "180px",
         border: "solid 1px #ffccdd",
         borderRadius: "10px",
+        alignSelf: "space-evenly",
       },
       beforeLast: {
          display: "flex",
-         justifyContent: "space-around", 
+         justifyContent: "space-around",
          alignItems: "center",
-         marginTop: "40px",
+         height: "90px",
       },
       loveIt: {
         height: "30px",
@@ -311,11 +287,13 @@ const Neumorphism = () => {
       farBottom: {
         display: "flex",
         justifyContent: "space-around",
-        alignItems: "center", 
-        marginTop: "120px",
+        alignItems: "center",
+        height: "80px",
         backgroundColor: "#ffccdd",
-        padding: "17px 0 25px 0",
-        borderRadius: "25px",
+        borderTopLeftRadius: "40px", 
+        borderTopRightRadius: "40px",
+        borderBottomLeftRadius: "20px", 
+        borderBottomRightRadius: "20px",
       },
 
       inBetween: {
@@ -324,8 +302,8 @@ const Neumorphism = () => {
         alignItems: "center",
       },
       repeat: {
-        height: "40px",
-        width: "40px",
+        height: "35px",
+        width: "35px",
         color: "rgba(42,27,137, .25)",
       },
       skipBack: {
@@ -360,9 +338,13 @@ const Neumorphism = () => {
         <div className="leftside" style={styles.leftside}>
           <div className="imageLeft" style={styles.imageLeft}>
             <div className="topControls" style={styles.topControls}>
-              <div className="arrow" style={styles.arrow}></div>
+              <div className="arrow">
+                <BsArrowLeftShort style={styles.arrow}/>
+              </div>
               <div className="playing" style={styles.playing}>PLAYING NOW</div>
-              <div className="menu" style={styles.menu}></div>
+              <div className="menu">
+                <CgMenuGridO style={styles.menu}/>
+              </div>
             </div>
             <div className="middleControls" style={styles.middleControls}>
               <div className="artistImage" style={styles.artistImage}></div>
@@ -377,9 +359,15 @@ const Neumorphism = () => {
               </div>
             </div>
             <div className="bottomControls" style={styles.bottomControls}>
-              <div className="rewind" style={styles.rewind}></div>
-              <div className="pause" style={styles.pause}></div>
-              <div className="forward" style={styles.forward}></div>
+              <div className="rewind">
+                <FiRewind style={styles.rewind}/>
+              </div>
+              <div className="pause">
+                <BsPauseFill style={styles.pause}/>
+              </div>
+              <div className="forward">
+                <FiFastForward style={styles.forward}/>
+              </div>
             </div>
           </div>
         </div>
@@ -404,8 +392,10 @@ const Neumorphism = () => {
                 </div>
                 <div className="middleControlsFar" style={styles.middleControlsFar}>
                     <div className="artistImageFar" style={styles.artistImageFar}></div>
-                    <h3 className="titleSongFar" style={styles.titleFar}>Wading</h3>
-                    <p className="artist" style={styles.artistFar}>Jhene Aiko</p>
+                </div>
+                <div className="album" style={styles.album}>
+                      <div className="titleSongFar" style={styles.titleFar}>Wading</div>
+                      <div className="artist" style={styles.artistFar}>Jhene Aiko</div>
                 </div>
                 <div className="timeLapseFar" style={styles.timeLapseFar}>
                     <div className="timeProgressFar" style={styles.timeProgressFar}>2:03</div>
